ad7546c911ada2341628a3e5e2a438fb7b50e6ae
zaidakhterr <zaidakhter1202@gmail.com>
2020-02-12 14:31:22 +0500

    Deploying to heroku

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use('/api/users', require('./routes/api/users'));
 //Serve static assets
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
